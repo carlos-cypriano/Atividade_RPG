c2ca7591a9372d9294213f4e46ca4b987a6d88dc
Alarket <Alarket@protonmail.com>
2021-07-24 19:59:30 -0300

    Update Monstro.java

---
+++ Batalha RPG/src/Monstro.java
@@ -0,0 +1,28 @@
+public class Monstro {
+    
+    int energia;
+    String nome;
+    int atk;
+
+    public Monstro(){
+
+    }
+
+    public void atacar(Humano h){
+
+    }
+
+    public void atacar(Monstro m){
+
+    }
+
+    public void defender(Humano h){
+
+    }
+
+    public void defender(Monstro m){
+
+    }
+
+}
+
