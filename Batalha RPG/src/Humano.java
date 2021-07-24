ab6a111c7e570e01a272e708ace00c39f4d110a9
Alarket <Alarket@protonmail.com>
2021-07-24 19:41:00 -0300

    Update Humano.java

---
+++ Batalha RPG/src/Humano.java
@@ -0,0 +1,28 @@
+public class Humano implements Acao{
+    
+    int energia;
+    String nome;
+    int atk;
+    int xp;
+
+    public Humano(){
+
+    }
+
+    public void atacar(Humano h){
+
+    }
+
+    public void atacar(Monstro m){
+
+    }
+
+    public void defender(Humano h){
+
+    }
+
+    public void defender(Monstro m){
+
+    }
+
+}
