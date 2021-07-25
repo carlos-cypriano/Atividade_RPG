b73f63367f58475a2665db0468fd71df26dca46b
carlos-cypriano <81194143+carlos-cypriano@users.noreply.github.com>
2021-07-25 17:12:23 -0300

    Criando novas subclasses
    Criei a classe Mago e Ladino, com os métodos sobrescritos atacar() e defender()

---
+++ Batalha RPG/src/Mago.java
@@ -0,0 +1,36 @@
+public class Mago extends Humano {
+    private int atk = 15;
+    private int defesa = 5;
+    private int energia = 100;
+    private String nome = "Mago";
+
+
+    //Criei esse Construtor pra validar os atributos acima
+    public Mago( int atk, int defesa, int energia, String nome){
+        this.atk = atk;
+        this.defesa = defesa;
+        this.energia = energia; 
+        this.nome = nome;
+    }
+
+    //@Override serve pra apontar que esse método está repetindo o método Pai no caso o Monstro
+    @Override
+    public void atacar (Humano h){
+        this.energia = this.energia - this.atk;
+    }
+
+    @Override
+    public void atacar (Monstro m){
+        this.energia = this.energia - this.atk;
+    }
+
+    @Override
+    public void defender (Humano h){
+        this.energia = this.energia - this.atk + this.defesa;
+    }
+
+    @Override
+    public void defender (Monstro m){
+        this.energia = this.energia - this.atk + this.defesa;
+    }
+}
