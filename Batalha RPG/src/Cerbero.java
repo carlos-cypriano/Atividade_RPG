a0ef7d381f5f1992816c97127192b16472aa58eb
carlos-cypriano <81194143+carlos-cypriano@users.noreply.github.com>
2021-07-24 21:12:15 -0300

    Alterando referência de classe para atributo da subclasse
    Alterei a referencia das classes Monstro e Humano na classe Cerbero para o atributo da própria classe Cerbero através do this que aponta pro atributo. Teoricamente não é pra dar erro mas futuramente a gente confere isso.

--- Batalha RPG/src/Cerbero.java
+++ Batalha RPG/src/Cerbero.java
@@ -17,21 +17,21 @@ public class Cerbero extends Monstro{
     //@Override serve pra apontar que esse método está repetindo o método Pai no caso o Monstro
     @Override
     public void atacar (Humano h){
-        h.energia = h.energia - this.atk;
+        this.energia = this.energia - this.atk;
     }
 
     @Override
     public void atacar (Monstro m){
-        m.energia = m.energia - this.atk;
+        this.energia = this.energia - this.atk;
     }
 
     @Override
     public void defender (Humano h){
-        h.energia = h.energia - this.atk + this.defesa;
+        this.energia = this.energia - this.atk + this.defesa;
     }
 
     @Override
     public void defender (Monstro m){
-        m.energia = m.energia - this.atk + this.defesa;
+        this.energia = this.energia - this.atk + this.defesa;
     }
 }
